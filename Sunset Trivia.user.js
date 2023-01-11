d60b0d7e48ecb7d27473f47ce0dd4756a6a4e839
Lance Held <lanceheld@users.noreply.github.com>
2023-01-11 21:56:21 +0000

    Add copy to clipboard

--- Sunset Trivia.user.js
+++ Sunset Trivia.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sunset Trivia
 // @namespace    https://sniff.org/
-// @version      0.1
+// @version      0.2
 // @description  Keeping trivia easy
 // @author       Lance Held
 // @match        https://triviarat.com/*
@@ -10,11 +10,14 @@
 // @grant        GM_getValue
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
+// @grant        GM_setClipboard
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js
 // @require      https://cdn.jsdelivr.net/combine/gh/lodash/lodash@4.17.11/dist/lodash.min.js,gh/lodash/lodash@4.17.11/dist/lodash.fp.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/uuid/8.3.2/uuidv4.min.js
 // @resource     SEMANTIC_UI_CSS https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css
+// @updateURL    https://github.com/lanceheld/tampermonkey-triviarat/blob/main/Sunset%20Trivia.user.js
+// @downloadURL  https://github.com/lanceheld/tampermonkey-triviarat/blob/main/Sunset%20Trivia.user.js
 // ==/UserScript==
 
 var $ = window.jQuery;
@@ -234,6 +237,11 @@ const renderForm = (store) => {
   </div>
   ${_.join('\n', _.map(renderRound(store), store.rounds))}
   <div class="item">
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M6 3C6 1.34315 7.34315 0 9 0H14C14.2652 0 14.5196 0.105357 14.7071 0.292893L21.7071 7.29289C21.8946 7.48043 22 7.73478 22 8V17C22 18.6569 20.6569 20 19 20H18V21C18 22.6569 16.6569 24 15 24H5C3.34315 24 2 22.6569 2 21V7C2 5.34315 3.34315 4 5 4H6V3ZM6 6H5C4.44772 6 4 6.44772 4 7V21C4 21.5523 4.44772 22 5 22H15C15.5523 22 16 21.5523 16 21V20H9C7.34315 20 6 18.6569 6 17V6ZM9 2C8.44772 2 8 2.44772 8 3V17C8 17.5523 8.44771 18 9 18H19C19.5523 18 20 17.5523 20 17V9H16C14.3431 9 13 7.65685 13 6V2H9ZM15 3.41421L18.5858 7H16C15.4477 7 15 6.55228 15 6V3.41421Z" fill="#293644"/>
+    </svg>  
+  </div>
+  <div class="item">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="20" width="20" class="trivia-clear" style="cursor: pointer">
       <path d="M0 0h24v24H0z" fill="none"/>
       <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" fill="#ffffff" />
@@ -268,6 +276,10 @@ const initForm = () => {
     $('.trivia-clear').on('click', () => {
         clearStore();
         initForm();
+    });
+
+    $('.trivia-copy').on('click', () => {
+      GM_setClipboard(store, 'json');
     });
 
     $('.trivia-category').on('input', function() {
